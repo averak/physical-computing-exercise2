6e723c6c0879aea9a8f5f792dbc3203f66aa687f
averak <abe12@mccc.jp>
2020-12-02 18:48:56 +0900

    [fix] model_pathを渡す

--- src/nnet/__init__.py
+++ src/nnet/__init__.py
@@ -9,16 +9,20 @@ class Model:
 
         from .make import make_nnet
 
-        self.__nnet = make_nnet((vec_length,), n_class, load_weights)
+        self.__nnet = make_nnet((vec_length,), n_class, load_weights, self.model_path)
 
     def train(self, x, y, save_weights=False):
         from .compile import compile_nnet
         from .train import train_nnet
 
         self.__nnet = compile_nnet(self.__nnet)
-        self.__nnet = train_nnet(self.__nnet, x, y, save_weights)
+        self.__nnet = train_nnet(self.__nnet, x, y, save_weights, self.model_path)
 
     def predict(self, x):
         from .predict import predict
 
         return np.argmax(predict(self.__nnet, x))
+
+    @property
+    def model_path(self):
+        return self.__model_path
--- src/tests/test_nnet.py
+++ src/tests/test_nnet.py
@@ -1,25 +1,25 @@
 import unittest
 import numpy as np
 import nnet
+from key_vector import VEC_SIZE
 
-VEC_LENGTH = 30
 N_CLASS = 10
 
 
 class TestNnet(unittest.TestCase):
     def setUp(self):
-        self.model = nnet.Model()
+        self.model = nnet.Model(model_file='test.h5')
 
     def test_train_nnet(self):
         # 適当なデータセットを作成
         x = []
         y = []
         for i in range(20):
-            x.append([np.random.rand() for _ in range(VEC_LENGTH)])
+            x.append([np.random.rand() for _ in range(VEC_SIZE)])
             y.append(i % N_CLASS)
 
-        self.model.train(x, y, True)
+        self.model.train(x, y, save_weights=True)
 
     def test_predict(self):
-        sample_x = [np.random.rand() for _ in range(VEC_LENGTH)]
+        sample_x = [np.random.rand() for _ in range(VEC_SIZE)]
         self.model.predict(sample_x)
