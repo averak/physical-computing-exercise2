73aa371cc0986a7cba2ad6701f2fb8df04d8fc07
averak <abe12@mccc.jp>
2021-01-04 01:21:55 +0900

    [test] test_key: KeyVectorオブジェクト

--- src/tests/test_key.py
+++ src/tests/test_key.py
@@ -1,17 +1,18 @@
 import unittest
 import numpy as np
 import key.vector as key_vector
+from key.vector import KeyVector
 from key.handler import KeyHandler
 
 
 class TestKey(unittest.TestCase):
+    def test_vector(self):
+        length = 30
+        key_vec = KeyVector(length)
+        self.assertEqual(key_vec.vector, [0] * length)
+
     def test_preprocessing(self):
         x = np.array([[1, 1, 1]])
         x = key_vector.preprocessing(x)[0]
         self.assertEqual(key_vector.VEC_SIZE, len(x))
         self.assertEqual([1 / 226], x[0])
-
-    def test_handler(self):
-        length = 30
-        key_handler = KeyHandler(length)
-        self.assertEqual(key_handler.keys, [0] * length)
